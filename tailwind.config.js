739a4fb8b4571a8dfdc85d6b82f841dff0fa67a7
Sa'dullayev Behruz <behruzsadullayev0913@gmail.com>
2025-12-16 17:26:44 +0500

    main qilinid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./js/**/*.js"],
+  content: [
+    "./html/**/*.html", // html papkasi ichidagi barcha html fayllar
+    "./js/**/*.js", // js papkasi ichidagi barcha js fayllar
+    "./index.html", // agar rootda ham html bo'lsa
+  ],
   theme: {
     extend: {},
   },
